7533c87481759aee3677f5c960cf5ab48194b71f
Masaki Kitsugi <mkitsugi250@gmail.com>
2024-09-13 13:22:21 +0900

    workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    distDir: 'out',
+    // distDir: 'out',
+    async rewrites() {
+        return [
+            {
+                source: '/:path*',
+                destination: '/:path*',
+            },
+        ];
+    },
 };
 
 export default nextConfig;
